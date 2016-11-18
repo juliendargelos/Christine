32db3f3aca1b5187aaa745b58e702f9563db7e70
Julien Dargelos <julien.dargelos@me.com>
2016-11-18 17:29:27 +0100

    Specify Amazon S3 endpoint

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,6 +79,7 @@ Rails.application.configure do
 
     config.paperclip_defaults = {
         storage: :s3,
+        s3_host_name: "s3-#{ENV['AWS_REGION']}.amazonaws.com",
         s3_credentials: {
             bucket: ENV.fetch('S3_BUCKET_NAME'),
             access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
@@ -86,8 +87,6 @@ Rails.application.configure do
             s3_region: ENV.fetch('AWS_REGION')
         }
     }
-
-    config.paperclip_defaults = { s3_host_name: "s3-#{ENV['AWS_REGION']}.amazonaws.com", }
 
     config.consider_all_requests_local = true
 end
